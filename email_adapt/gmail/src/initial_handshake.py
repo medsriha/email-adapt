dd97dcffc70245a522c001e0c6b2bb9d5d33a65a
medsriha <medsriha@gmail.com>
2025-01-12 21:25:18 -0600

    fix document store

--- email_adapt/gmail/src/initial_handshake.py
+++ email_adapt/gmail/src/initial_handshake.py
@@ -8,13 +8,13 @@ import logging
 from typing import List, Dict, Any
 from urllib.parse import quote_plus
 
-from email_adapt.database.vector_store import VectorStoreBase
+from email_adapt.database.vector_store import VectorStore
 from email_adapt.gmail.src.extract_body import ExtractUserBodyFromEmail
 from email_adapt.gmail.src.api.threads import GmailThreadExtractor
 from email_adapt.gmail.src.user_profile import UserProfile
 
 # Configure logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 logger = logging.getLogger(__name__)
 
 
@@ -34,7 +34,8 @@ class InitialHandshake:
         self.email_address = email_address
         self.safe_email_address = quote_plus(email_address)
         # Use the vector store for RAG purposes
-        self.vector_store = VectorStoreBase(collection_name=self.safe_email_address)
+        self.vector_store = VectorStore(collection_name=self.safe_email_address, overwrite=True)
+        self.user_profile = UserProfile(vector_store=self.vector_store)
 
     def get_threads(self, max_results: int = 100) -> List[Dict[str, Any]]:
         """
@@ -42,13 +43,13 @@ class InitialHandshake:
 
         :param max_results: Maximum number of threads to retrieve
         """
-        logger.debug(f"Retrieving up to {max_results} threads")
+        logger.debug(f"[{self.__class__.__name__}] Retrieving up to {max_results} threads")
         try:
             threads = GmailThreadExtractor(email_address=self.email_address).get_threads(max_results=max_results)
-            logger.info(f"Successfully retrieved {len(threads)} threads")
+            logger.info(f"[{self.__class__.__name__}] Successfully retrieved {len(threads)} threads")
             return threads
         except FileNotFoundError as e:
-            logger.error(f"Credentials file not founds: {str(e)}")
+            logger.error(f"[{self.__class__.__name__}] Credentials file not founds: {str(e)}")
             raise
 
     def get_email_bodies(self, threads: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
@@ -61,10 +62,10 @@ class InitialHandshake:
         try:
             body_extractor = ExtractUserBodyFromEmail(email_address=self.email_address)
             email_bodies = body_extractor.extract(threads)
-            logger.info(f"Successfully extracted {len(email_bodies)} email bodies")
+            logger.info(f"[{self.__class__.__name__}] Successfully extracted {len(email_bodies)} email bodies")
             return email_bodies
         except Exception as e:
-            logger.error(f"Failed to extract email bodies: {str(e)}")
+            logger.error(f"[{self.__class__.__name__}] Failed to extract email bodies: {str(e)}")
             raise
 
     def index_email_bodies(self, email_bodies: List[Dict[str, Any]]) -> None:
@@ -97,32 +98,11 @@ class InitialHandshake:
             ]
 
             self.vector_store.index(texts, metadata)
-            logger.info(f"Successfully indexed {len(texts)} email bodies")
+            logger.info(f"[{self.__class__.__name__}] Successfully indexed {len(texts)} email bodies")
         except Exception as e:
-            logger.error(f"Failed to index email bodies: {str(e)}")
+            logger.error(f"[{self.__class__.__name__}] Failed to index email bodies: {str(e)}")
             raise
 
-    def create_profile(self, weight_recency: float = 1.0, weight_length: float = 1.0) -> None:
-        """
-        Create and index user profile based on email analysis.
-
-        :param weight_recency: Weight factor for recent emails
-        :param weight_length: Weight factor for email length
-        :param overwrite: Whether to overwrite the existing collection. This is for development purposes.
-        """
-        try:
-
-            user_profile = UserProfile(collection_name=self.safe_email_address)
-            user_profile.create_user_profile(
-                metadata_filter={"is_forwarded": False},
-                top_k=100,
-                weight_recency=weight_recency,
-                weight_length=weight_length
-            )
-            logger.info("Successfully created and indexed user profile")
-        except Exception as e:
-            logger.error(f"Failed to create user profile: {str(e)}")
-            raise
     
     def __call__(self) -> None:
         """
@@ -134,7 +114,19 @@ class InitialHandshake:
         # Store the email bodies in the vector database
         self.index_email_bodies(email_bodies = self.get_email_bodies(threads))
         # Create the user profile. Pick the top_k most recent emails.
-        self.create_profile(top_k=30, weight_recency=1.0, weight_length=0)
+        try:
+            self.user_profile.create_user_profile(
+                metadata_filter={"is_forwarded": False},
+                top_k=30,
+                weight_recency=1.0,
+                weight_length=0.0
+            )
+            logger.info(f"[{self.__class__.__name__}] Successfully created {self.safe_email_address} profile")
+        except Exception as e:
+            logger.error(f"[{self.__class__.__name__}] Failed to create {self.safe_email_address} profile: {str(e)}")
+            raise
+
 
 if __name__ == "__main__":
-    InitialHandshake(email_address="medsriha@gmail.com")()
+    l = InitialHandshake(email_address="medsriha@gmail.com")
+    l()
